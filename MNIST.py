cd56b58c42949cec731425ed6f938138c87796ac
SivaShankar1255 <155956695+AlabanuSivaShankar@users.noreply.github.com>
2024-12-02 17:24:46 +0530

    Update MNIST.py

--- MNIST.py
+++ MNIST.py
@@ -1,4 +1,7 @@
 # Import necessary libraries
+import os
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
@@ -11,31 +14,37 @@ import matplotlib.pyplot as plt
 st.title("MNIST Digit Classification with CNN")
 
 # Load the MNIST dataset
-@st.cache_resource  # Cache the model to avoid reloading on each interaction
-def load_data_and_model():
-    # Preprocess data
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+@st.cache_resource
+def load_data():
+    # Load and preprocess data
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255.0
     x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255.0
     y_train = tf.keras.utils.to_categorical(y_train, 10)
     y_test = tf.keras.utils.to_categorical(y_test, 10)
+    return x_train, y_train, x_test, y_test
 
-    # Build and train the CNN model
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        MaxPooling2D(pool_size=(2, 2)),
-        Dropout(0.2),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(pool_size=(2, 2)),
-        Dropout(0.2),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dropout(0.3),
-        Dense(10, activation='softmax')
-    ])
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=5, batch_size=128, validation_data=(x_test, y_test))
-    return model, x_test, y_test
+x_train, y_train, x_test, y_test = load_data()
+
+# Build the model outside the cache
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(pool_size=(2, 2)),
+    Dropout(0.2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Dropout(0.2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.3),
+    Dense(10, activation='softmax')
+])
+
+# Compile and train the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, batch_size=128, validation_data=(x_test, y_test))
+
+return model, x_test, y_test
 
 model, x_test, y_test = load_data_and_model()
 
